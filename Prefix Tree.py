6f73a28cb06a7fa6bf27d8a16ce098f3d2482637
LAVANYA B Y <lavanyalavanya0555@gmail.com>
2025-11-29 12:02:44 +0530

    Add files via upload

---
+++ Prefix Tree.py
@@ -0,0 +1,27 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for ch in word:
+            node = node.children.setdefault(ch, TrieNode())
+        node.end = True
+
+    def search(self, word):
+        node = self.root
+        for ch in word:
+            if ch not in node.children:
+                return False
+            node = node.children[ch]
+        return node.end
+
+t = Trie()
+t.insert("hello")
+print(t.search("hello"))
+print(t.search("hell"))
