a00856e6d5e6229eac9518423de5abd019ddc405
LAVANYA B Y <lavanyalavanya0555@gmail.com>
2025-12-03 16:12:08 +0530

    Add files via upload

---
+++ Insertion Sort.py
@@ -0,0 +1,11 @@
+arr = [9,3,1,5,2]
+
+for i in range(1, len(arr)):
+    key = arr[i]
+    j = i-1
+    while j >= 0 and arr[j] > key:
+        arr[j+1] = arr[j]
+        j -= 1
+    arr[j+1] = key
+
+print(arr)
