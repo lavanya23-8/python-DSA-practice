264f5be481c9fdbb1047623ca00b6a4c5512035d
LAVANYA B Y <lavanyalavanya0555@gmail.com>
2025-12-11 19:47:53 +0530

    Add files via upload

---
+++ LinkedList.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def display(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+        print("None")
+
+# Example usage
+ll = LinkedList()
+ll.insert("A")
+ll.insert("B")
+ll.insert("C")
+ll.display()
+
